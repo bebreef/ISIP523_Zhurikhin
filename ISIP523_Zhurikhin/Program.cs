4685b1277b186e32c74230443ab031b06322d4c0
bebreef <zurihinruslan@gmail.com>
2025-09-22 16:29:33 +0300

    сделан класс, ну это вообще класс

--- ISIP523_Zhurikhin/Program.cs
+++ ISIP523_Zhurikhin/Program.cs
@@ -1,2 +1,15 @@
-﻿
+﻿class Product
+{
+    public int ProductID;
+    public string name;
+    public double price;
+    public int quantity;
+    public bool instock;
+    enum category
+    {
+        Food,
+        Clothes,
+        Tech
+    }
+}
 
