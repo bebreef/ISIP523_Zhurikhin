e8b75a1644028402237dd38a3c59c183cc5cc91f
bebreef <zurihinruslan@gmail.com>
2025-09-22 16:38:47 +0300

    mehню

--- ISIP523_Zhurikhin/Program.cs
+++ ISIP523_Zhurikhin/Program.cs
@@ -1,4 +1,6 @@
-﻿class Product
+﻿using static System.Net.Mime.MediaTypeNames;
+
+class Product
 {
     public int ProductID;
     public string name;
@@ -7,9 +9,52 @@
     public bool instock;
     enum category
     {
-        Food,
+        Food=1,
         Clothes,
         Tech
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool cont = true;
+        while (cont)
+        {
+            Console.WriteLine("\n=== МЕНЮ ===");
+            Console.WriteLine("1. Добавить товар");
+            Console.WriteLine("2. Удалить товар");
+            Console.WriteLine("3. Заказать поставку товара");
+            Console.WriteLine("4. Продать товар");
+            Console.WriteLine("5. Поиск по названию");
+            Console.WriteLine("0. Выход");
+            Console.Write("Выберите пункт меню: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "0":
+                    Console.WriteLine("Завершение программы");
+                    return;
+                default:
+                    Console.WriteLine("Неверный выбор");
+                    break;
+            }
+            Console.WriteLine("Хотите вернуться в меню? (1-да, 0-нет)");
+            string end = Console.ReadLine();
+            cont = (end == "1");
+        }
     }
 }
 
