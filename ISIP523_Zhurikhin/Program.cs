f696ca1b5f9d38b6f7d6ccd90d72b83c9a3c2a37
bebreef <zurihinruslan@gmail.com>
2025-10-13 15:19:18 +0300

    взял за основу практики пример из теории, добавил тестовые данные для проверки

--- ISIP523_Zhurikhin/Program.cs
+++ ISIP523_Zhurikhin/Program.cs
@@ -1,2 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum cat
+{
+    Philosophy = 1,
+    Anatomy,
+    Mathematics
+}
+class Person
+{
+    private string FIO;
+    private DateOnly Birthday; 
+    private string Gender;
+
+    public Person(string fio, DateOnly birthday, string gender)
+    {
+        FIO = fio;
+        Birthday = birthday;
+        Gender = gender;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine($"FIO: {FIO}\nBirthday: {Birthday}\nGender: {Gender} ");
+    }
+}
+
+class Student : Person
+{
+    private int StudentNumberID;
+    private bool PCExperience;
+    private cat[] course;
+    private string TheHealthGroup;
+
+    public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, cat[] course)
+        : base(fio, birthday, gender) 
+    {
+        StudentNumberID = id;
+        this.PCExperience = PCExperience;
+        this.course = course;
+        this.TheHealthGroup = TheHealthGroup;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Student");
+        base.Print();
+        Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\nTheHealthGroup: {TheHealthGroup}");
+        Console.Write("Courses: ");
+        for (int i = 0; i < course.Length; i++)
+        {
+            Console.Write(course[i]);
+            if (i < course.Length - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("\n");
+    }
+}
+class Teacher : Person
+{
+    private cat Subject;
+    private int ExperienceYears;
+
+    public Teacher(string fio, DateOnly birthday, string gender, cat subject, int PCExperience)
+        : base(fio, birthday, gender)
+    {
+        Subject = subject;
+        ExperienceYears = PCExperience;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Teacher");
+        base.Print();
+        Console.WriteLine($"Subject: {Subject}\nExperienceYears: {ExperienceYears}\n");
+    }
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+     Student student1 = new Student(
+    id: 1001,
+    fio: "Иванов Иван Иванович",
+    birthday: new DateOnly(2003, 5, 15),
+    gender: "Мужской",
+    PCExperience: true,
+    TheHealthGroup: "Основная",
+    course: new cat[] {(cat)1}
+    );
+    Student student2 = new Student(
+    id: 1002,
+    fio: "Петрова Анна Сергеевна",
+    birthday: new DateOnly(2002, 8, 22),
+    gender: "Женский",
+    PCExperience: false,
+    TheHealthGroup: "Подготовительная",
+    course: new cat[] { (cat)2, (cat)3 }
+    );
+    Student student3 = new Student(
+    id: 1003,
+    fio: "Сидоров Алексей Петрович",
+    birthday: new DateOnly(2004, 1, 10),
+    gender: "Мужской",
+    PCExperience: true,
+    TheHealthGroup: "Специальная",
+    course: new cat[] {(cat)1, (cat)2}
+    );
+    Teacher teacher1 = new Teacher(
+    fio: "Козлова Мария Владимировна",
+    birthday: new DateOnly(1980, 3, 12),
+    gender: "Женский",
+    subject: (cat)3,
+    PCExperience: 15
+    );
+    Teacher teacher2 = new Teacher(
+    fio: "Николаев Дмитрий Сергеевич",
+    birthday: new DateOnly(1975, 11, 5),
+    gender: "Мужской",
+    subject: (cat)1,
+    PCExperience: 20
+    );
+    Teacher teacher3 = new Teacher(
+    fio: "Васильева Елена Игоревна",
+    birthday: new DateOnly(1990, 7, 30),
+    gender: "Женский",
+    subject: (cat)2,
+    PCExperience: 8
+    );
+    }
+}
+
