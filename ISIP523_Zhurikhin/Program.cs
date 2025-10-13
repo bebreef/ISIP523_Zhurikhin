d75159084678bd229b0a4a94d349b73b636e007d
bebreef <zurihinruslan@gmail.com>
2025-10-13 16:11:20 +0300

    пришлось убрать перечисление впринципе и делать класс для курса

--- ISIP523_Zhurikhin/Program.cs
+++ ISIP523_Zhurikhin/Program.cs
@@ -1,13 +1,29 @@
-﻿public enum cat
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Course
 {
-    Philosophy = 1,
-    Anatomy,
-    Mathematics
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Course(int id, string name, string description = "")
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
+
 class Person
 {
     private string FIO;
-    private DateOnly Birthday; 
+    private DateOnly Birthday;
     private string Gender;
 
     public Person(string fio, DateOnly birthday, string gender)
@@ -16,127 +32,121 @@ class Person
         Birthday = birthday;
         Gender = gender;
     }
+
     public virtual void Print()
     {
-        Console.WriteLine($"FIO: {FIO}\nBirthday: {Birthday}\nGender: {Gender} ");
+        Console.WriteLine($"ФИО: {FIO}\nДата рождения: {Birthday}\nПол: {Gender}");
     }
+
+    public string GetFIO() => FIO;
+    public DateOnly GetBirthday() => Birthday;
+    public string GetGender() => Gender;
 }
 
 class Student : Person
 {
     private int StudentNumberID;
     private bool PCExperience;
-    private cat[] course;
+    private List<Course> courses;
     private string TheHealthGroup;
 
-    public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, cat[] course)
-        : base(fio, birthday, gender) 
+    public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, Course[] course)
+        : base(fio, birthday, gender)
     {
         StudentNumberID = id;
         this.PCExperience = PCExperience;
-        this.course = course;
+        this.courses = new List<Course>(course);
         this.TheHealthGroup = TheHealthGroup;
     }
 
     public override void Print()
     {
-        Console.WriteLine("Student");
+        Console.WriteLine("=== СТУДЕНТ ===");
         base.Print();
-        Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\nTheHealthGroup: {TheHealthGroup}");
-        Console.Write("Courses: ");
-        for (int i = 0; i < course.Length; i++)
+        Console.WriteLine($"ID студента: {StudentNumberID}\nОпыт работы с ПК: {PCExperience}\nГруппа здоровья: {TheHealthGroup}");
+        Console.Write("Курсы: ");
+        if (courses.Count == 0)
         {
-            Console.Write(course[i]);
-            if (i < course.Length - 1)
-                Console.Write(", ");
+            Console.WriteLine("нет курсов");
         }
-        Console.WriteLine("\n");
+        else
+        {
+            for (int i = 0; i < courses.Count; i++)
+            {
+                Console.Write(courses[i].Name);
+                if (i < courses.Count - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
     }
+
+    public void AddCourse(Course course)
+    {
+        if (!courses.Any(c => c.Id == course.Id))
+        {
+            courses.Add(course);
+            Console.WriteLine($"Курс '{course.Name}' добавлен студенту {GetFIO()}");
+        }
+        else
+        {
+            Console.WriteLine($"Студент {GetFIO()} уже записан на курс '{course.Name}'");
+        }
+    }
+
+    public int GetStudentNumberID() => StudentNumberID;
+    public bool GetPCExperience() => PCExperience;
+    public List<Course> GetCourses() => courses;
+    public string GetHealthGroup() => TheHealthGroup;
 }
+
 class Teacher : Person
 {
-    private cat Subject;
+    private Course Subject;
     private int ExperienceYears;
 
-    public Teacher(string fio, DateOnly birthday, string gender, cat subject, int PCExperience)
+    public Teacher(string fio, DateOnly birthday, string gender, Course subject, int experienceYears)
         : base(fio, birthday, gender)
     {
         Subject = subject;
-        ExperienceYears = PCExperience;
+        ExperienceYears = experienceYears;
     }
 
     public override void Print()
     {
-        Console.WriteLine("Teacher");
+        Console.WriteLine("=== ПРЕПОДАВАТЕЛЬ ===");
         base.Print();
-        Console.WriteLine($"Subject: {Subject}\nExperienceYears: {ExperienceYears}\n");
+        Console.WriteLine($"Предмет: {Subject.Name}\nСтаж работы: {ExperienceYears} лет\n");
     }
+
+    public Course GetSubject() => Subject;
+    public int GetExperienceYears() => ExperienceYears;
 }
+
 class Program
 {
     static List<Student> students = new List<Student>();
     static List<Teacher> teachers = new List<Teacher>();
-    static int ID = 4;
+    static List<Course> courses = new List<Course>();
+    static int studentID = 3;
+    static int teacherID = 3;
+    static int courseID = 3;
+
     static void Main(string[] args)
     {
-     Student student1 = new Student(
-    id: 1,
-    fio: "Иванов Иван Иванович",
-    birthday: new DateOnly(2003, 5, 15),
-    gender: "Мужской",
-    PCExperience: true,
-    TheHealthGroup: "Основная",
-    course: new cat[] {(cat)1}
-    );
-    Student student2 = new Student(
-    id: 2,
-    fio: "Петрова Анна Сергеевна",
-    birthday: new DateOnly(2002, 8, 22),
-    gender: "Женский",
-    PCExperience: false,
-    TheHealthGroup: "Подготовительная",
-    course: new cat[] { (cat)2, (cat)3 }
-    );
-    Student student3 = new Student(
-    id: 3,
-    fio: "Сидоров Алексей Петрович",
-    birthday: new DateOnly(2004, 1, 10),
-    gender: "Мужской",
-    PCExperience: true,
-    TheHealthGroup: "Специальная",
-    course: new cat[] {(cat)1, (cat)2}
-    );
-    Teacher teacher1 = new Teacher(
-    fio: "Козлова Мария Владимировна",
-    birthday: new DateOnly(1980, 3, 12),
-    gender: "Женский",
-    subject: (cat)3,
-    PCExperience: 15
-    );
-    Teacher teacher2 = new Teacher(
-    fio: "Николаев Дмитрий Сергеевич",
-    birthday: new DateOnly(1975, 11, 5),
-    gender: "Мужской",
-    subject: (cat)1,
-    PCExperience: 20
-    );
-    Teacher teacher3 = new Teacher(
-    fio: "Васильева Елена Игоревна",
-    birthday: new DateOnly(1990, 7, 30),
-    gender: "Женский",
-    subject: (cat)2,
-    PCExperience: 8
-    );
+        InitializeData();
         bool cont = true;
         while (cont)
         {
-            Console.WriteLine("\n=== МЕНЮ ===");
-            Console.WriteLine("1. Добавить cтудента");
+            Console.WriteLine("\n=== ГЛАВНОЕ МЕНЮ ===");
+            Console.WriteLine("1. Добавить студента");
             Console.WriteLine("2. Добавить преподавателя");
-            Console.WriteLine("3. Добавить курс"); 
+            Console.WriteLine("3. Добавить курс");
             Console.WriteLine("4. Вывод информации по студентам");
-            Console.WriteLine("5. Вывод информации по преподавателям"); 
+            Console.WriteLine("5. Вывод информации по преподавателям");
             Console.WriteLine("6. Вывод информации по курсам");
+            Console.WriteLine("7. Добавить курс к студенту");
             Console.WriteLine("0. Выход");
             Console.Write("Выберите пункт меню: ");
 
@@ -145,16 +155,25 @@ class Program
             switch (choice)
             {
                 case "1":
+                    AddStudent();
                     break;
                 case "2":
+                    AddTeacher();
                     break;
                 case "3":
+                    AddCourse();
                     break;
                 case "4":
+                    PrintStudentsInfo();
                     break;
                 case "5":
+                    PrintTeachersInfo();
                     break;
                 case "6":
+                    PrintCoursesInfo();
+                    break;
+                case "7":
+                    AddCourseToStudent();
                     break;
                 case "0":
                     Console.WriteLine("Завершение программы");
@@ -163,10 +182,173 @@ class Program
                     Console.WriteLine("Неверный выбор");
                     break;
             }
-            Console.WriteLine("Хотите вернуться в меню? (1-да, 0-нет)");
-            string end = Console.ReadLine();
-            cont = (end == "1");
+
+            if (choice != "0")
+            {
+                Console.WriteLine("\nХотите вернуться в меню? (1-да, 0-нет)");
+                string end = Console.ReadLine();
+                cont = (end == "1");
+            }
         }
+    }
+
+    static void InitializeData()
+    {
+        Course philosophy = new Course(1, "Философия", "Изучение основных философских концепций");
+        Course anatomy = new Course(2, "Анатомия", "Изучение строения человеческого тела");
+        Course mathematics = new Course(3, "Математика", "Изучение математических дисциплин");
+
+        courses.Add(philosophy);
+        courses.Add(anatomy);
+        courses.Add(mathematics);
+
+        Student student1 = new Student(
+            id: 1,
+            fio: "Иванов Иван Иванович",
+            birthday: new DateOnly(2003, 5, 15),
+            gender: "Мужской",
+            PCExperience: true,
+            TheHealthGroup: "Основная",
+            course: new Course[] { philosophy }
+        );
+
+        Student student2 = new Student(
+            id: 2,
+            fio: "Петрова Анна Сергеевна",
+            birthday: new DateOnly(2002, 8, 22),
+            gender: "Женский",
+            PCExperience: false,
+            TheHealthGroup: "Подготовительная",
+            course: new Course[] { anatomy, mathematics }
+        );
+
+        Student student3 = new Student(
+            id: 3,
+            fio: "Сидоров Алексей Петрович",
+            birthday: new DateOnly(2004, 1, 10),
+            gender: "Мужской",
+            PCExperience: true,
+            TheHealthGroup: "Специальная",
+            course: new Course[] { philosophy, anatomy }
+        );
+
+        Teacher teacher1 = new Teacher(
+            fio: "Козлова Мария Владимировна",
+            birthday: new DateOnly(1980, 3, 12),
+            gender: "Женский",
+            subject: mathematics,
+            experienceYears: 15
+        );
+
+        Teacher teacher2 = new Teacher(
+            fio: "Николаев Дмитрий Сергеевич",
+            birthday: new DateOnly(1975, 11, 5),
+            gender: "Мужской",
+            subject: philosophy,
+            experienceYears: 20
+        );
+
+        Teacher teacher3 = new Teacher(
+            fio: "Васильева Елена Игоревна",
+            birthday: new DateOnly(1990, 7, 30),
+            gender: "Женский",
+            subject: anatomy,
+            experienceYears: 8
+        );
+
+        students.Add(student1);
+        students.Add(student2);
+        students.Add(student3);
+        teachers.Add(teacher1);
+        teachers.Add(teacher2);
+        teachers.Add(teacher3);
+    }
+    static void AddStudent()
+    {
+        Console.WriteLine("\n=== Добавление студента ===");
+        studentID++;
+
+        Console.Write("Введите ФИО: ");
+        string fio = Console.ReadLine();
+
+        Console.Write("Введите дату рождения (гггг-мм-дд): ");
+        DateOnly birthday = DateOnly.Parse(Console.ReadLine());
+
+        Console.Write("Введите пол: ");
+        string gender = Console.ReadLine();
+
+        Console.Write("Есть ли опыт работы с ПК (true/false): ");
+        bool pcExperience = bool.Parse(Console.ReadLine());
+
+        Console.Write("Введите группу здоровья: ");
+        string healthGroup = Console.ReadLine();
+
+        Console.WriteLine("\nДоступные курсы:");
+        PrintCoursesInfo();
+
+        Console.Write("Введите ID курсов через запятую: ");
+        string[] courseIds = Console.ReadLine().Split(',');
+
+        List<Course> studentCourses = new List<Course>();
+        foreach (string idStr in courseIds)
+        {
+            if (int.TryParse(idStr.Trim(), out int courseId))
+            {
+                Course course = courses.FirstOrDefault(c => c.Id == courseId);
+                if (course != null)
+                {
+                    studentCourses.Add(course);
+                }
+            }
+        }
+
+        Student newStudent = new Student(
+            studentID,
+            fio,
+            birthday,
+            gender,
+            pcExperience,
+            healthGroup,
+            studentCourses.ToArray()
+        );
+
+        students.Add(newStudent);
+        Console.WriteLine($"\nСтудент {fio} успешно добавлен с ID: {studentID}!");
+    }
+
+    static void AddTeacher()
+    {
+        Console.WriteLine("\n=== Добавление преподавателя ===");
+        teacherID++;
+
+        Console.Write("Введите ФИО: ");
+        string fio = Console.ReadLine();
+
+        Console.Write("Введите дату рождения (гггг-мм-дд): ");
+        DateOnly birthday = DateOnly.Parse(Console.ReadLine());
+
+        Console.Write("Введите пол: ");
+        string gender = Console.ReadLine();
+
+        Console.WriteLine("\nДоступные предметы:");
+        PrintCoursesInfo();
+
+        Console.Write("Введите ID предмета: ");
+        int subjectId = int.Parse(Console.ReadLine());
+        Course subject = courses.FirstOrDefault(c => c.Id == subjectId);
+
+        if (subject == null)
+        {
+            Console.WriteLine("Предмет с таким ID не найден!");
+            return;
+        }
+
+        Console.Write("Введите стаж работы (лет): ");
+        int experience = int.Parse(Console.ReadLine());
+
+        Teacher newTeacher = new Teacher(fio, birthday, gender, subject, experience);
+        teachers.Add(newTeacher);
+        Console.WriteLine($"\nПреподаватель {fio} успешно добавлен!");
     }
 }
 
