7c6a10cc5b85ac06c4e6998cfe8a3f23397319b0
bebreef <zurihinruslan@gmail.com>
2025-11-23 21:33:01 +0300

    создал классы и папки

---
+++ ISIP523_Zhurikhin/Model/Enemies/Mage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP523_Zhurikhin.Model.Enemies
+{
+    internal class Mage
+    {
+    }
+}
