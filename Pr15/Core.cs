3e4240749e9f1bd6028c98d5be37740d8d366df9
bebreef <zurihinruslan@gmail.com>
2026-02-16 10:02:22 +0300

    подключена бд

---
+++ Pr15/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr15
+{
+    internal class Core
+    {
+        public static PR15Entities Context = new PR15Entities();
+    }
+}
